f61c028b7e144933670d3cf9af16f974b1a02ac6
Haroldo Gamal <haroldo.gamal@silexonline.org>
2020-04-10 20:35:54 -0300

    Introdução de tabelas editáveis pelo usuário de Lâminas e Carretéis Alteração do formato de saída Algumas otimizações

---
+++ CoilForm.py
@@ -0,0 +1,53 @@
+import csv
+
+class CoilForm:
+	src = ""
+	model = ""
+	type = ""
+	obs = ""
+	leg = 0.0
+	stack = 0.0
+	length = 0.0
+	thickness = 0.0
+
+	def __init__(self, src, model, leg, stack, lenght, thickness, type, obs):
+		self.supplier = src
+		self.model = model
+		self.type = type
+		self.obs = obs
+		self.leg = float(leg)
+		self.stack = float(stack)
+		self.length = float(lenght)
+		self.thickness = float(thickness)
+
+	def __repr__(self):
+		rc =  "model=\"" + self.supplier + ":" + self.model + "\", type=\"" + self.type + "\", leg=" + str(self.leg)
+		rc = rc + " mm, stack=" + str(self.stack) + " mm, thickness=" + str(self.thickness) + " mm"
+		return rc
+    
+	@staticmethod
+	def load(file):
+		table = []
+		with open(file) as csv_file:
+			csv_reader = csv.reader(csv_file, delimiter=',')
+			line_count = 0
+			for row in csv_reader:
+				if line_count == 0:
+					# print(f'Column names: {", ".join(row)}')
+					line_count += 1
+				else:
+					bob = CoilForm(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
+					table.append(bob);
+					# print(f'\t{row[0]}: code="{row[1]}"", leg={row[2]} mm')
+					line_count += 1
+			# print(f'Processed {line_count} lines.')
+			return table
+
+	@staticmethod
+	def lookupCoilForms(table, sizemin, sizemax):
+		resp = []
+		for c in table:
+			# print(c, size)
+			if sizemin <= c.leg and sizemax >= c.leg:
+				resp.append(c)
+		return resp
